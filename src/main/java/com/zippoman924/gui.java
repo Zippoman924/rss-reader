6630256c9e89a948cebd8ace59d89fb9e4790b6a
[Brian Hurley] <[hurleybri924@gmail.com]>
2023-12-28 00:02:08 -0500

    Buttons with basic actions have been added.

--- src/main/java/com/zippoman924/gui.java
+++ src/main/java/com/zippoman924/gui.java
@@ -1,8 +1,8 @@
 package com.zippoman924;
 
 import javax.swing.*;
-import java.awt.Dimension;
-import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class gui {
     /**
@@ -18,26 +18,54 @@ public class gui {
       JFrame frame = new JFrame("RSS Reader");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+      //Set window size & position
+      int WindowWidth = 800;
+      int WindowHeight = 800;
+
+      frame.setSize(WindowWidth,WindowHeight);
+      frame.setLocationRelativeTo(null);  //Openes center of screen
+
+      //START adding elements to the window. 
+
+      /*
       //Create the "Hello World" label.
       JLabel label = new JLabel("Hello World! How are you?");
 
       //Sets font and alignment to center of the window. 
       label.setHorizontalAlignment(SwingConstants.CENTER);
       label.setFont(new Font("Serif", Font.PLAIN, 40));
-      frame.getContentPane().add(label);
+      frame.add(label);
+      */
 
-      //Create & set minimum window size
-      int WindowHeight = 800;
-      int WindowWidth = 800;
+      //Creates an "add feed" button
+      JButton addButton = new JButton("Add New RSS Feed");
+      //TODO: Might be a good idea to make these positions dynamic and not hard coded.
+      addButton.setBounds(300, 100, 200, 80);
+      frame.add(addButton);
+      addButton.addActionListener(new ActionListener() {
 
-      Dimension dims = new Dimension(WindowWidth, WindowHeight);
-      frame.setMinimumSize(dims);
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          System.out.println("Add Button has been pressed!");
+        }
+        
+      });
 
-      //Set window to open in the center of the screen
-      frame.setLocationRelativeTo(null);
+      //Creates an "remove feed" button
+      JButton removeButton = new JButton("Remove RSS Feed");
+      removeButton.setBounds(300, 300, 200, 80);
+      frame.add(removeButton);
+      removeButton.addActionListener(new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          System.out.println("Remove Button has been pressed!");
+        }
+        
+      });
 
       //Display the window.
-      frame.pack();
+      frame.setLayout(null);
       frame.setVisible(true);
   }
 }
